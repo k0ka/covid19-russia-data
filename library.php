cfcac028b11e61952611f2e85e42ae115095cb75
k0ka <koka@idwrx.com>
2020-04-18 00:46:45 +0200

    parse webarchive

--- library.php
+++ library.php
@@ -1,5 +1,91 @@
 <?php
 
+require_once 'simple_html_dom.php';
+require_once 'regions.php';
+
+/**
+ * Parses today data from site
+ *
+ * @throws Exception
+ */
+function ParseToday()
+{
+    // https://стопкоронавирус.рф/
+    $url = 'https://xn--80aesfpebagmfblc0a.xn--p1ai/';
+    $date = new DateTime('now');
+    ParseStopCoronavirusUrl($url, $date);
+}
+
+/**
+ * Parses date from webarchive
+ *
+ * @param DateTime $date
+ * @throws Exception
+ */
+function ParseWebArchive($date){
+    $url = 'https://web.archive.org/web/20200416044534/https://xn--80aesfpebagmfblc0a.xn--p1ai/';
+    $tomorrow = clone $date;
+    $tomorrow->modify("+1 DAY");
+
+    $webArchiveTimeUrl = 'https://web.archive.org/__wb/calendarcaptures/2?url=https%3A%2F%2Fxn--80aesfpebagmfblc0a.xn--p1ai%2F&date=' . $tomorrow->format('Ymd');
+    $result = UploadUrl($webArchiveTimeUrl);
+
+    $items = json_decode($result, true);
+    $time = $items['items'][0][0];
+    if(!$time){
+        echo "Can't find time for date " . $tomorrow->format('Ymd');
+        exit(1);
+    }
+
+    $webArchiveUrl = 'https://web.archive.org/web/' . $tomorrow->format('Ymd') . sprintf('%06d', $time) . '/https://xn--80aesfpebagmfblc0a.xn--p1ai/';
+
+    ParseStopCoronavirusUrl($webArchiveUrl, $date);
+}
+
+/**
+ * Parses url from site or webarchive and saves to file
+ *
+ * @param $url
+ * @param DateTime $date
+ * @throws Exception
+ */
+function ParseStopCoronavirusUrl($url, $date)
+{
+    $regions = new Regions();
+
+    $html = UploadUrl($url);
+    $dom = str_get_html($html);
+
+    $element = $dom->find('.d-map__list', 0);
+    if($element == null){
+        echo "No d-map__list found\n";
+        exit(1);
+    }
+
+    $result = "Region Id,Sick,Healed,Die,Region Name";
+    $sumSick = 0;
+    $sumHealed = 0;
+    $sumDie = 0;
+    foreach ($element->find('table tr') as $regionData){
+        /** @var simple_html_dom_node $regionData */
+        $regionName = trim($regionData->find('th', 0)->plaintext);
+        $sick = intval($regionData->find('td', 0)->plaintext);
+        $healed = intval($regionData->find('td', 1)->plaintext);
+        $die = intval($regionData->find('td', 2)->plaintext);
+
+        $sumSick += $sick;
+        $sumHealed += $healed;
+        $sumDie += $die;
+
+        $regionId = $regions->GetRegionId($regionName);
+        $result .= "$regionId,$sick,$healed,$die,$regionName\n";
+    }
+
+    $result .= "99,$sumSick,$sumHealed,$sumDie,Всего\n";
+    file_put_contents("data/{$date->format('Y-m-d')}.csv", $result);
+}
+
+
 /**
  * Uploads url with info
  *
--- parse.php
+++ parse.php
@@ -1,41 +1,15 @@
 <?php
 
 require_once 'library.php';
-require_once 'simple_html_dom.php';
-require_once 'regions.php';
 
-$regions = new Regions();
+ParseToday();
 
-// https://стопкоронавирус.рф/
-$url = 'https://xn--80aesfpebagmfblc0a.xn--p1ai/';
-
-$html = UploadUrl($url);
-$dom = str_get_html($html);
-
-$element = $dom->find('.d-map__list', 0);
-if($element == null){
-    echo "No d-map__list found\n";
-    exit(1);
+/*
+$date = new DateTime('2020-04-15');
+for ($i = 0; $i < 30; $i++){
+    echo $date->format('Y-m-d') . "\n";
+    ParseWebArchive($date);
+    $date->modify('-1 DAY');
 }
+*/
 
-$result = "Region Id,Sick,Healed,Die,Region Name";
-$sumSick = 0;
-$sumHealed = 0;
-$sumDie = 0;
-foreach ($element->find('table tr') as $regionData){
-    /** @var simple_html_dom_node $regionData */
-    $regionName = trim($regionData->find('th', 0)->plaintext);
-    $sick = intval($regionData->find('td', 0)->plaintext);
-    $healed = intval($regionData->find('td', 1)->plaintext);
-    $die = intval($regionData->find('td', 2)->plaintext);
-
-    $sumSick += $sick;
-    $sumHealed += $healed;
-    $sumDie += $die;
-
-    $regionId = $regions->GetRegionId($regionName);
-    $result .= "$regionId,$sick,$healed,$die,$regionName\n";
-}
-
-$result .= "99,$sumSick,$sumHealed,$sumDie,Всего\n";
-file_put_contents('data/' . date('Y-m-d') . '.csv', $result);
